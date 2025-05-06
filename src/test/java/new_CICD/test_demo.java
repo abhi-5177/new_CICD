713b1d407a034cd2164ec8dccc9bcada3ab0b51f
abhi-5177 <abhiptl5177@gmail.com>
2025-05-06 12:03:12 +0530

    Initial commit

--- src/test/java/new_CICD/test_demo.java
+++ src/test/java/new_CICD/test_demo.java
@@ -24,8 +24,22 @@ public class test_demo {
 	    @Test
 	    public void testwikiSearchLinux() {
 	        driver.get("https://www.java.com/en/");
-	        AssertJUnit.assertEquals(driver.getTitle(), "Java | Oracle1");  
+	        //AssertJUnit.assertEquals(driver.getTitle(), "Java | Oracle1");  
+	        String actualTitle = driver.getTitle();
+	        String expectedTitle = "The123Java";
+	        AssertJUnit.assertEquals("Expected title: " + expectedTitle + " but found: " + actualTitle, actualTitle, expectedTitle);
+
 	        
+	        //06052025---10:47PM
+	    }
+	    @Test
+	    public void visit_geeksforgeeks() {
+	        driver.get("https://www.geeksforgeeks.org/");
+	        //AssertJUnit.assertEquals(driver.getTitle(), "Java | Oracle");  
+	        String actualTitle = driver.getTitle();
+	        String expectedSubstring = "GeeksforGeeks";
+	        AssertJUnit.assertTrue("Expected title to contain: " + expectedSubstring + " but found: " + actualTitle, actualTitle.contains(expectedSubstring));
+
 	        
 	        //06052025---10:47PM
 	    }
